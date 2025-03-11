33ab269051f9196d6d7f927b96b98d3c79a3d31e
HJ2608 <98634463+HJ2608@users.noreply.github.com>
2025-03-11 06:10:05 +0000

    date-time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,22 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+
+//date is an object
+
+// let myCreateDate = new Date(2023, 1, 23)
+//let myCreateDate = new Date(2023, 1, 23, 5, 3)
+let myCreateDate = new Date("2023-01-14")
+//console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Date.now()/1000);
+
+
